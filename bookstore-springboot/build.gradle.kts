1b0c83c9dd1f7672da9deded13ecb0ca956cdc6f
Alexandros Koufatzis <alexandros.koufatzis@arconsis.com>
2022-11-20 00:02:47 +0200

    Update Spring Boot to 3.0.0 RC2

--- bookstore-springboot/build.gradle.kts
+++ bookstore-springboot/build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.0-RC1"
+	id("org.springframework.boot") version "3.0.0-RC2"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("org.graalvm.buildtools.native") version "0.9.17"
 	kotlin("jvm") version "1.7.20"
 	kotlin("plugin.spring") version "1.7.20"
 }
