9ddd87a87d364305ffea2a91967d4a1d1caeef35
Alexandros Koufatzis <alexandros.koufatzis@arconsis.com>
2022-12-03 10:56:20 +0200

    Update Spring Boot to 3.0.0

--- bookstore-springboot/build.gradle.kts
+++ bookstore-springboot/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.0-RC2"
+	id("org.springframework.boot") version "3.0.0"
 	id("io.spring.dependency-management") version "1.1.0"
 	id("org.graalvm.buildtools.native") version "0.9.17"
 	kotlin("jvm") version "1.7.21"
@@ -14,7 +14,6 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
