8d01daaa36a86a243b68c7236c8ef24851ea2012
Tomislav Eric <hi@tomislav.mobi>
2023-09-11 09:01:34 +0200

    run migration on start

--- bookstore-rocketrs/src/main.rs
+++ bookstore-rocketrs/src/main.rs
@@ -6,16 +6,17 @@ mod routes;
 extern crate rocket;
 use crate::models::repo::Repo;
 use db::setup;
-use migration::{Migrator, MigratorTrait};
+use migration::{cli, Migrator};
 use routes::books::*;
 
 #[launch]
 async fn rocket() -> _ {
     let db = match setup::set_up_db().await {
         Ok(db) => db,
-        Err(err) => panic!("{}", err),
+        Err(err) => panic!("Could not connect: {}", err),
     };
     
+    cli::run_cli(Migrator).await;
     rocket::build()
         .mount(
             "/",
